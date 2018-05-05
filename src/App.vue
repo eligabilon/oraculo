2a46cde28744a154b62ed95971f1f5b0104aebf7
Reginaldo Santos <rmsantos@br.ibm.com>
2018-05-04 22:20:41 -0300

    Oraculo - vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="O grande Oráculo"/>
+    <Question />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Question from './components/Question'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Question
   }
 }
 </script>
---
+++ src/components/Question.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <h2>Faça uma pergunta cuja resposta seja SIM ou NÃO</h2>
+        <form>
+            Pergunta: <input type="text" placeholder="Você gosta deste app?" name="pergunta" />
+            <input type="submit" value="Enviar" v-on:click="perguntaOraculo">
+        </form> 
+        <Answer  v-if="oraculoDisse && oraculoDisse.answer" :resposta="oraculoDisse"/>
+        <br />
+        <input type="submit" value="De novo" v-on:click="reinicia" />
+    </div>
+    
+</template>
+
+
+
+<script>
+import axios from 'axios'
+import Answer from './Answer'
+
+export default {
+    name: "Question",
+    components: {
+        Answer
+    },
+    data: function() {
+        return {
+            oraculoDisse: null
+        }
+
+    },
+    methods: {
+        perguntaOraculo: function(e) {
+            e.preventDefault()
+            axios
+                .get('https://yesno.wtf/api')
+                .then(response => {
+                    console.log(response)
+                    this.oraculoDisse = response.data
+                })
+        }, 
+        reinicia: function(e) {
+            this.oraculoDisse = nulls
+        }
+    }
+}
+</script>
+
+
+
+<style>
+
+</style>
